c53261f679e4eae611c35e5a88016ffc8e12916f
Fernando Felício <fernando_felicio82@yahoo.com.br>
2017-12-27 02:00:28 -0200

    Finalização da aula 3.

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -4,6 +4,51 @@ var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(quantidadePalavras);
 
 var campo = $('.campo-digitacao');
+var tempoRestante = $("#tempo-digitacao").text();
+
+/**
+ * Tratamento para o tempo restante
+ */
+/**
+ * Na função abaixo não foi utilizado o evento 'click', porque o mesmo não mapeia o campo caso o usuário acesse o input através
+ * do 'TAB'(tabulação). No caso, optou-se pelo evento 'focus', porque ele é usado para detectar quando o usuário entra em um campo,
+ * não necessariamente digitando.
+ * 
+ * No campo está sendo executada a função 'one'. Ela serve para escutar o evento somente uma vez.
+ * A função .one() possuí a mesma sintaxe da função .on().
+ * A função 'on' não seria a melhor alternativa, porque se o campo perdesse o foco e retornasse o foco no mesmo, a função
+ * seria executada a todo momento, podendo acarretar outros bugs.
+ */ 
+campo.one("focus", function() {
+    /**
+     * A função 'setInterval' é própria do Javascript.
+     * Ela faz com que uma determinada ação (passada como primeiro parâmetro),
+     * seja executada em um intervalo de tempo (passado como segundo parâmetro, no nosso caso, 1 segundo, ou 1000 milissegundos).
+     * 
+     * Toda função 'setInterval' *retorna o seu próprio id*.
+     * No caso, iremos utilizar a referência da mesma, para parar a sua execução quando o tempo tiver terminado.
+     */
+    var cronometroID = setInterval(function() {
+        tempoRestante--;
+        $("#tempo-digitacao").text(tempoRestante);
+
+        // Verifica se o tempo acabou para poder desabilitar o textarea.
+        if (tempoRestante < 1) {
+            /**
+             * Abaixo é utilizada a função 'attr'. Ela serve para resgatar alguma propriedade ou setar algum valor no campo.
+             * Exemplo de resgate do atributo 'rows': campo.attr("rows");
+             * Exemplo de alteração no atributo 'rows' do textarea: campo.attr("rows", 500);
+             */
+            campo.attr("disabled", true);
+
+            /**
+             * A função abaixo serve para parar a execução da função 'setInterval'.
+             * Ela recebe como argumento, o id da função 'setInterval'.
+             */
+            clearInterval(cronometroID);
+        }
+    }, 1000);
+});
 
 /**
  * O evento 'input' mapeia tudo que o usuário digitou.
