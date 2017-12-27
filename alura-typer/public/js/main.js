94ce51356b6973f049dc09a20103d3132ab643e3
Fernando Felício <fernando_felicio82@yahoo.com.br>
2017-12-27 00:41:39 -0200

    Finalização da aula 2

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -2,3 +2,25 @@ var frase = $('.frase').text();
 var quantidadePalavras = frase.split(' ').length;
 var tamanhoFrase = $("#tamanho-frase");
 tamanhoFrase.text(quantidadePalavras);
+
+var campo = $('.campo-digitacao');
+
+/**
+ * O evento 'input' mapeia tudo que o usuário digitou.
+ */
+campo.on('input', function() {
+    var conteudo = campo.val();
+
+    var qtdCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtdCaracteres);
+
+    /**
+     * Para contar mais precisamente temos que utilizar uma expressão regular no lugar do espaço vazio,
+     * uma expressão regular que busca qualquer caractere, exceto espaço vazio, essa expressão é mostrada abaixo.
+     * 
+     * Agora os espaços não são mais considerados como palavras, mas a contagem sempre mostra a quantidade de palavras mais uma,
+     * para resolver isso vamos subtrair um do length do conteúdo.
+     */
+    var qtdPalavras = conteudo.split(/\S+/).length - 1;
+    $("#contador-palavras").text(qtdPalavras);
+});
